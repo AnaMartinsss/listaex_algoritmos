ed36c5f8cbc04c04622600d4f2685ba5f81a4bc0
AnaMartinsss <martinsanalaura108@gmail.com>
2024-11-09 12:54:30 -0300

    att

--- ex6.py
+++ ex6.py
@@ -1,1 +1,7 @@
-print(f"***GERADOR DE TABUADA!***")
+#(Cálculo de Média Aritmética) Peça três notas de um aluno e calcule a média aritmética  delas.
+print(f"***CALCULO DE MÉDIA ARITMÉTICA!***")
+notas = [float(input("Digite a primeira nota: ")), 
+         float(input("Digite a segunda nota: ")), 
+         float(input("Digite a terceira nota: "))]
+media = sum(notas) / len(notas)
+print(f"A média é: {media}") 
